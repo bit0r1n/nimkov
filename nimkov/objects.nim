5411da090016a45e39035b0ee1fc9760a68cca03
bit0r1n <me@bitor.in>
2022-08-02 19:45:49 +0300

    fix: remove formatter from objects

--- nimkov/objects.nim
+++ nimkov/objects.nim
@@ -1,18 +1,15 @@
 import options
 from ./typedefs import MarkovGenerateOptions
 from ./validators import defaultValidator
-from ./formatters import defaultFormatter
 
 proc newMarkovGenerateOptions*(
         attempts = 1;
         begin = none string;
         validator = defaultValidator();
-        formatter = defaultFormatter
     ): MarkovGenerateOptions =
     ## Creates an options for generator.
     result = MarkovGenerateOptions(
         attempts: attempts,
         begin: begin,
-        validator: validator,
-        formatter: formatter
+        validator: validator
     )
